1b19b8ba01e60389932b7386746064b581a16d9c
Swayam Gaur <goura0775@gmail.com>
2026-04-02 17:00:07 +0530

    ghj

--- src/controllers/indiamartController.js
+++ src/controllers/indiamartController.js
@@ -5,13 +5,30 @@ export const receiveLead = async (req, res) => {
         console.log("IndiaMART Lead:", req.body);
 
         // 👇 mapping karna (important)
+        // const leadData = {
+        //     name: req.body.SENDER_NAME || req.body.name,
+        //     mobile: req.body.SENDER_MOBILE || req.body.mobile,
+        //     email: req.body.SENDER_EMAIL || "",
+        //     product: req.body.QUERY_PRODUCT_NAME || "",
+        //     message: req.body.QUERY_MESSAGE || "",
+        //     city: req.body.SENDER_CITY || "",
+        //     source: "IndiaMART"
+        // };
+
+
         const leadData = {
-            name: req.body.SENDER_NAME || req.body.name,
-            mobile: req.body.SENDER_MOBILE || req.body.mobile,
+            name: req.body.SENDER_NAME || "",
+            mobile: req.body.SENDER_MOBILE || "",
             email: req.body.SENDER_EMAIL || "",
             product: req.body.QUERY_PRODUCT_NAME || "",
             message: req.body.QUERY_MESSAGE || "",
             city: req.body.SENDER_CITY || "",
+            state: req.body.SENDER_STATE || "",
+            company: req.body.SENDER_COMPANY || "",
+            pincode: req.body.SENDER_PINCODE || "",
+            queryType: req.body.QUERY_TYPE || "",
+            queryTime: req.body.QUERY_TIME || "",
+            uniqueId: req.body.UNIQUE_QUERY_ID || "",
             source: "IndiaMART"
         };
 
